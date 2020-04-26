e041116cdb88d0954cc3abcc1bbc7462c61172a6
rafasera <rafael.filgue@gmail.com>
2020-04-25 23:37:13 -0300

    Criado a pagina de cadastrar usuarios

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,10 @@
+{{-- Essa view é somente para fazer a gestao cadastro e eliminacao de usuarios  --}}
+@extends('layouts.app')
+
+@section('content')
+{{-- criar um formulario em cima e logo abaixo a view com os dados fazer normal usando os recursos do laravel sem criar uma view --}}
+
+
+
+{{-- Fim do conteudo --}}
+@endsection
