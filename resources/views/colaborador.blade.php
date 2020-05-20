4c79849e6fb178e0c0398499d2560e30b4353ed5
rafasera <rafael.filgue@gmail.com>
2020-05-20 00:11:13 -0300

    adicionado diversos links e solucionado opções conforme nivel do usuario

--- resources/views/colaborador.blade.php
+++ resources/views/colaborador.blade.php
@@ -10,30 +10,32 @@
     </div>
     @endif
 
-    <div class="content container">
+    <div class="container">
         <h3>Painel de controle</h3>
         @foreach ($adms as $adm)
 
 
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Escala de Plantão</a>
-            <a href="https://blog.laravel.com">Cadastro Plantão</a>
-            <a href="https://laracasts.com">Dados Pessoais</a>
+        <ul class="list-group col-md-8 my-4">
+            <li class="list-group-item active"><a href="#" class="text-decoration-none text-dark">Escala de Plantão</a>
+            </li>
+            <li class="list-group-item"><a href="/plantao" class="text-decoration-none text-dark">Cadastro
+                    Plantão</a></li>
+            <li class="list-group-item"><a href="/usuario" class="text-decoration-none text-dark">Dados
+                    Pessoais</a></li>
             {{-- Essa vai ser para admin --}}
             @if ( $adm->adm == 1 || $adm->adm == 3 )
-            <a href="https://laravel-news.com">Controle de Usuario</a>
+            <li class="list-group-item"><a href="/usuarios" class="text-decoration-none text-dark">Controle de
+                    Usuario</a></li>
             @endif
             {{-- Somente super admin ver como acessar --}}
             @if ( $adm->adm == 3 )
-            <a href="https://nova.laravel.com">Admin</a>
+            <li class="list-group-item"><a href="/home/admin" class="text-decoration-none text-dark">Admin</a>
+            </li>
             @endif
+        </ul>
+        @endforeach
 
-            {{-- Esse cara tera acesso a parte de gestão empresa --}}
 
-            @endforeach
-
-        </div>
 
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,10 +64,10 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/colaborador">Painel de controle</a>
+                                <a class="dropdown-item" href="/usuario">Dados pessoais</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+                                                document.getElementById('logout-form').submit();">Sair</a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                     style="display: none;">
